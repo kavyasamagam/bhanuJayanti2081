02de4d9938961e45e5a05549157913c2dfe19a35
codeninja <adarsha.pau@gmail.com>
2024-07-04 23:23:49 +0545

    complete frontend

---
+++ tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "FFF8DB",
+        secondary: "#64748b",
+        dark: "FFF078",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+};
